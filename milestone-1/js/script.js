fbb25c48584f57ee6841e909081aa1965122a0e9
Deki97 <dejan97@libero.it>
2021-12-10 17:43:26 +0100

    milestone-1 done

--- milestone-1/js/script.js
+++ milestone-1/js/script.js
@@ -1,8 +1,8 @@
 // Milestone 1
 
-// Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi   CSS diverse
+// [x] Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi   CSS diverse
 
-// Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+// [x] Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
 
 Vue.config.devtools = true;
 
