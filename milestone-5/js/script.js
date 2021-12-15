1af36b4761dd30bbe8cc2f18021f0d9fec7220e1
Deki97 <dejan97@libero.it>
2021-12-15 15:06:11 +0100

    milestone-5

--- milestone-5/js/script.js
+++ milestone-5/js/script.js
@@ -24,6 +24,7 @@ const app = new Vue(
             myMessage: '',
             activeContact: 0,
             searchResult: '',
+            activeMessage: 0,
             contacts: [
                 {
                     name: 'Michele',
